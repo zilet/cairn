690ee8c984294f8f784c8ca35d0ee287d87225f4
Milos Zikic <milos@spicefactory.co>
2026-07-01 05:14:47 -0400

    Tighten Health Picture dependency type

--- src/client/me-health-screen.ts
+++ src/client/me-health-screen.ts
@@ -195,7 +195,7 @@ function healthReadDeps(): ClientHealthReadControllerDeps {
   };
 }
 
-function healthPictureDeps() {
+function healthPictureDeps(): ClientHealthPictureControllerDeps {
   return {
     root: view,
     state,
